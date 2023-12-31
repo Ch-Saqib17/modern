65b2274d50ddc9bd0f3d6a2e13747ba86b627af0
Ch <chsaqib11223@gmail.com>
2023-12-31 23:28:06 +0500

    add

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,15 +1,9 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-12-31'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+export const dataset = "asgyrgwu"
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+export const projectId = "production"
 
 export const useCdn = false
 
